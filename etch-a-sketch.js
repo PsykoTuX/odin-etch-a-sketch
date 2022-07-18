68c15f54921b4387ac4af219441f03acdb213b0e
Arthur <arthur.guilbert2708@gmail.com>
2022-07-17 21:25:08 -0400

    Add a button that creates a popup asking for dimensions of new grid

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -3,33 +3,46 @@ let container = document.getElementById('container');
 let div = document.createElement('div');
 div.classList = 'cell';
 
+let clear = document.getElementById('clear');
+let changeDimension = document.getElementById('changeDimension');
+let divs = document.querySelectorAll("div.cell");
+let erase = document.querySelectorAll('.cell');
 
+function generateGrid () {
+    let size = prompt ("Choose the dimension of the grid: up to 100 squares per side.");
+    
 
-for (let i=0; i<(16*16); i++) {
-    container.appendChild(div.cloneNode());
+    for (let i=0; i<(size*size); i++) {
+        container.appendChild(div.cloneNode());
+    }
+
+    container.style.setProperty('grid-template-columns', `repeat(${size}, 1fr)`);
+    container.style.setProperty('grid-template-rows',    `repeat(${size}, 1fr)`);
+
+    divs = document.querySelectorAll("div.cell");
+    divs.forEach((div) => {
+        div.addEventListener('mouseover',() => {
+            div.classList.add('toggled')
+        })
+    });
+
 }
-
-const divs = document.querySelectorAll("div.cell");
-
-
-divs.forEach((div) => {
-    div.addEventListener('mouseover',() => {
-div.classList.add('toggled')
-
-    })
-
-});
 
 
 function reset () {
     divs.forEach((div) => {
     div.classList.remove('toggled')
-        })
+        });
     };
 
-let clear = document.getElementById('clear');
+
 
 
 clear.addEventListener('click', () => {
 reset ()
+})
+
+changeDimension.addEventListener('click', () => {
+reset ()
+generateGrid()
 })
