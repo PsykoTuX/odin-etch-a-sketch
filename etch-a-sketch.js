db363f9a149a84bd8e152ee4e5b177c693adc14d
Arthur <arthur.guilbert2708@gmail.com>
2022-07-18 13:05:02 -0400

    Add comments to the JS script and cleanup code

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,14 +1,15 @@
-let container = document.getElementById('container');
+//Declare variables
 
+let container = document.getElementById('container');
 let div = document.createElement('div');
 div.classList = 'cell';
-
 let clear = document.getElementById('clear');
 let changeDimension = document.getElementById('changeDimension');
 let divs = document.querySelectorAll("div.cell");
 let erase = document.getElementsByClassName('cell');
 let size = 0
 
+// Set up the grid that appears on the landing page
 function initialGrid (initialSize) {
     for (let i=0; i<(initialSize*initialSize); i++) {
         container.appendChild(div.cloneNode());
@@ -24,9 +25,11 @@ function initialGrid (initialSize) {
         })
     });
 }
+//Call the initial grid function (16x16)
+initialGrid(16);
 
-initialGrid(16)
 
+// Function permitting to change the dimensions of the grid.
 function generateGrid () {
     size = prompt ("Choose the dimension of the grid: up to 100 squares per side.");
     size = parseInt(size);
@@ -35,28 +38,29 @@ function generateGrid () {
         generateGrid();
     }
     else {
-    for (let i= erase.length-1; i>=0; --i) {
-        erase[i].remove();
-    }
+        for (let i= erase.length-1; i>=0; --i) {
+            erase[i].remove();
+        }
+        for (let i=0; i<(size*size); i++) {
+            container.appendChild(div.cloneNode());
+        }
 
-    for (let i=0; i<(size*size); i++) {
-        container.appendChild(div.cloneNode());
-    }
+        //Adjust the CSS grid template to the dimensions of the grid.
+        container.style.setProperty('grid-template-columns', `repeat(${size}, 1fr)`);
+        container.style.setProperty('grid-template-rows',    `repeat(${size}, 1fr)`);
 
-    container.style.setProperty('grid-template-columns', `repeat(${size}, 1fr)`);
-    container.style.setProperty('grid-template-rows',    `repeat(${size}, 1fr)`);
-
-    divs = document.querySelectorAll("div.cell");
-    divs.forEach((div) => {
-        div.addEventListener('mouseover',() => {
-            div.classList.add('toggled')
-        })
-    });
+        //Add the hovering effect ('activate' the cells)
+        divs = document.querySelectorAll("div.cell");
+        divs.forEach((div) => {
+            div.addEventListener('mouseover',() => {
+                div.classList.add('toggled')
+            })
+        });
 }
 
 }
 
-
+//Reset the cells to their initial (blank state)
 function reset () {
     divs.forEach((div) => {
     div.classList.remove('toggled')
@@ -64,13 +68,12 @@ function reset () {
 
     };
 
-
-
-
+//Call the above function by clicking on the clear button.
 clear.addEventListener('click', () => {
 reset ()
 })
 
+//Call the generateGrid function by clicking on the Change dimensions button. 
 changeDimension.addEventListener('click', () => {
 reset ()
 generateGrid()
