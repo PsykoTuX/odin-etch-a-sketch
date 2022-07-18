d81aad0e5a8bbc870f95da61241b2e1a3a81310e
Arthur <arthur.guilbert2708@gmail.com>
2022-07-18 12:51:46 -0400

    Make the prompt for dimensions only accept integers between 0 and 100

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -7,9 +7,34 @@ let clear = document.getElementById('clear');
 let changeDimension = document.getElementById('changeDimension');
 let divs = document.querySelectorAll("div.cell");
 let erase = document.getElementsByClassName('cell');
+let size = 0
+
+function initialGrid (initialSize) {
+    for (let i=0; i<(initialSize*initialSize); i++) {
+        container.appendChild(div.cloneNode());
+    }
+
+    container.style.setProperty('grid-template-columns', `repeat(${initialSize}, 1fr)`);
+    container.style.setProperty('grid-template-rows',    `repeat(${initialSize}, 1fr)`);
+
+    divs = document.querySelectorAll("div.cell");
+    divs.forEach((div) => {
+        div.addEventListener('mouseover',() => {
+            div.classList.add('toggled')
+        })
+    });
+}
+
+initialGrid(16)
 
 function generateGrid () {
-    let size = prompt ("Choose the dimension of the grid: up to 100 squares per side.");
+    size = prompt ("Choose the dimension of the grid: up to 100 squares per side.");
+    size = parseInt(size);
+    if ((size>100 || size <0 || Number.isInteger(size)==false))  {
+        alert('What you typed was not an integer comprised between 0 and 100, please specify the dimensions again.');
+        generateGrid();
+    }
+    else {
     for (let i= erase.length-1; i>=0; --i) {
         erase[i].remove();
     }
@@ -27,6 +52,7 @@ function generateGrid () {
             div.classList.add('toggled')
         })
     });
+}
 
 }
 
